e4e8dcb3bf5dd690c198736155255c06013fcb21
Synixxal <69634563+Synixxal@users.noreply.github.com>
2020-10-04 11:32:17 +0800

    Delete linkvertise.php

--- bypasses/linkvertise.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if (PHP_SAPI === 'cli') {
-        $url = $argv[1];
-    }
-    else {
-        $url = $_GET['url'];
-    }
-    echo file_get_contents('https://ovd-center.eu/API/LinkvertiseBypass.php?url='.str_replace("/","(",$url));
-?>
